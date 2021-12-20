c003039aa2c4c4b36a5ec2f0a276ae470b7b9e62
wataridmm <r.watari.watari@gmail.com>
2021-12-20 18:28:16 +0900

    chapter06

--- JavaWcp/src/Chapter05.java
+++ JavaWcp/src/Chapter05.java
@@ -3,28 +3,25 @@ public class Chapter05 {
 	public static void main(String... args) {
 		long l = 100000000L;
 		System.out.println(l);
-		
+
 		double d = 2.39D;
 		System.out.println(d);
-		
+
 		boolean b = false;
 		System.out.println(b);
-		
-		
+
 		Float f = 8.348f;
 		System.out.println(f);
-		
+
 		Integer i = 100;
 		System.out.println(i);
-		
+
 		Character c = 'お';
 		System.out.println(c);
-		
+
 		int[] array = { 0, 10, 20, 30, 40 };
 		System.out.println(array[3]);
-		
-		
-		
+
 	}
 
 }
