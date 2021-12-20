390f9d5231df8c2df4c36f3b38a627dbf33a7792
wataridmm <r.watari.watari@gmail.com>
2021-12-20 15:50:53 +0900

    chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,17 @@
+
+public class Chapter04 {
+	public static void main(String... args) {
+		String name;
+		name = "watari";
+		System.out.println(name);
+		
+		name = "piano";
+		System.out.println(name);
+		
+		final int luck;
+		luck = 7;
+		System.out.println(luck);
+				
+	}
+
+}
