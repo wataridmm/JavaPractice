52019fd00712bf97b4ffcfda537e7c7cd3b60311
wataridmm <r.watari.watari@gmail.com>
2022-01-11 23:41:43 +0900

    add

--- JavaWcp/src/Chapter08.java
+++ JavaWcp/src/Chapter08.java
@@ -9,6 +9,11 @@ public class Chapter08 {
 		int array[] = { 1, 4, 14, 25 };
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
+			
+			
+			
+			
+			
 		}
 		for (int val : array) {
 			if (val % 2 == 0) {
